d829a2701f69acedb888472143821aca99ffc226
Niklas Eicker <nikle1@gmx.de>
2018-03-17 01:40:16 +0100

    move files out of gamebox project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Match It
+
+Match It is an inventory game for the Spigot plugin [GameBox]. It is distributed in the GameBox jar which is published on [Spigot](https://www.spigotmc.org/resources/37273/).
+
+Match It is a memory game and comes in three different grid sizes small, medium and large. All items and messages are configurable.
+
+![medium match it game](medium_game.png "Half finished, medium MatchIt game")
+
+[GameBox]: https://github.com/NiklasEi/GameBox
---
+++ src/main/java/me/nikl/matchit/MIGameRule.java
@@ -0,0 +1,26 @@
+package me.nikl.matchit;
+
+import me.nikl.gamebox.data.toplist.SaveType;
+import me.nikl.gamebox.game.rules.GameRuleRewards;
+
+/**
+ * Created by nikl on 02.12.17.
+ */
+public class MIGameRule extends GameRuleRewards {
+    private double timeVisible;
+    private MatchIt.GridSize gridSize;
+
+    public MIGameRule(boolean saveStats, double cost, String key, MatchIt.GridSize gridSize, double timeVisible, double moneyToWin, int tokenToWin) {
+        super(key, saveStats, SaveType.TIME_LOW, cost, moneyToWin, tokenToWin);
+        this.gridSize = gridSize;
+        this.timeVisible = timeVisible;
+    }
+
+    public MatchIt.GridSize getGridSize() {
+        return gridSize;
+    }
+
+    public double getTimeVisible() {
+        return timeVisible;
+    }
+}
